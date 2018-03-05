4961f9c1abccaaac518665b508a8eaf3aa996a29
trattner <trattner@mit.edu>
2018-03-05 08:11:09 -0500

    replicating local server ping

--- http-server.js
+++ http-server.js
@@ -1,7 +1,11 @@
 var http = require('http');
+var url = require('url');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.name + " " + q.email;
+  console.log(txt);
+  res.end(txt);
 }).listen(8080); //the server object listens on port 8080
